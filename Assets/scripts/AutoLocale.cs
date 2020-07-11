b527cbf9589d0a8135409df229b5c733d0600873
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2020-07-11 13:53:01 -0300

    Update Project

---
+++ Assets/scripts/AutoLocale.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class AutoLocale : MonoBehaviour
+{
+    static Dictionary<string, string[]> strings = null;
+    static int currentLang = 0;
+    static List<AutoLocale> autoLocales;
+
+
+    public string key;
+    private Text textComponent;
+
+    static void ParseCSV() {
+        AutoLocale.strings = new Dictionary<string, string[]>();
+        TextAsset textFile =(TextAsset)Resources.Load("strings");
+        string text = textFile.text;
+        print(text);
+        string[] lines = text.Split('\n');
+        foreach (string line in lines) {
+            string[] words = line.Trim().Split(',');
+            string key = words[0];
+            print(key);
+            string[] value = new string[words.Length - 1];
+            Array.Copy(words, 1, value, 0, words.Length - 1);
+            AutoLocale.strings.Add(key, value);
+        }
+    }
+
+    static string GetString(string key) {
+        if (AutoLocale.strings == null) {
+            AutoLocale.ParseCSV();
+        }
+        string[] values = null;
+        AutoLocale.strings.TryGetValue(key, out values);
+        if (values == null) {
+            return key;
+        } else {
+            return values[AutoLocale.currentLang];
+        }
+    }
+
+    public static void ChangeLanguage(int language) {
+        AutoLocale.currentLang = language;
+        if (AutoLocale.autoLocales == null) return;
+        foreach (AutoLocale text in AutoLocale.autoLocales) {
+            text.UpdateText();
+        }
+    }
+
+    void Awake() {
+        if (autoLocales == null) {
+            autoLocales = new List<AutoLocale>();
+        }
+        autoLocales.Add(this);
+        textComponent = gameObject.GetComponent<Text>();
+        UpdateText();
+    }
+
+    void UpdateText() {
+        textComponent.text = AutoLocale.GetString(key);
+    }
+}
---
+++ Assets/scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private int currentLang = 0;
+
+    void Awake() {
+        AutoLocale.ChangeLanguage(currentLang);
+    }
+
+    public void ChangeLanguage(int language) {
+        currentLang = language;
+        AutoLocale.ChangeLanguage(language);
+    }
+}
